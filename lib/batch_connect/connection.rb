df45a10ea45b074370bc04edb2865c3b466f996b
Jeremy Nicklas <jnicklas@osc.edu>
2016-07-22 06:36:25 -0400

    add error if yml is missing

--- lib/batch_connect/connection.rb
+++ lib/batch_connect/connection.rb
@@ -8,12 +8,17 @@ module BatchConnect
     # Template path for batch scripts
     self.template_path = File.expand_path('../../../templates/connections', __FILE__)
 
-    # @param yml [Pathname, #to_s] path to the yaml file with connection information
+    # An exception raised when attempting to resolve a yml file that doesn't exist
+    class MissingYmlFile < StandardError; end
+
+    # @param yml [#to_s] path to the yaml file with connection information
+    # @raise [MissingYmlFile] if the requested yml file doesn't exist
     def initialize(yml:)
       yml = Pathname.new(yml).expand_path
       Dir.open(yml.dirname).close # flush NFS cache
 
       # Parse yaml
+      raise MissingYmlFile, "cannot find yml file #{yml}" unless yml.file?
       YAML.load_file(yml).each do |k, v|
         self[k] = v
       end
